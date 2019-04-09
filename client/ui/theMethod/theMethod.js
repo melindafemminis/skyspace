ced8ab301fea318f1dd57f98757cb2f078f34be9
Melinda <femminis.mda@gmail.com>
2019-04-09 14:18:05 +0200

    theMethod: change color when clicked
    Changer la couleur des bouttons cliqués avec event.currentTarget !

--- client/ui/theMethod/theMethod.js
+++ client/ui/theMethod/theMethod.js
@@ -14,34 +14,36 @@ var answered = {
   }
 
   var numberOfButtons = 5;
-  var compteur = 0;
+  var compteur = 1;
 
   Template.theMethod.events({
     'click .thebtn'(event, instance) {
-        event.preventDefault();  
-        //insert code here
-        // Update state on button click
-         answered["button" + this._id] = true;
-        // Have all buttons been clicked?
-        var allTrue = Object.keys(answered).every(function(key){ return answered[key] === true });
-        if (allTrue) {
-        // Remove a button and reset state
-        document.getElementById("button" + numberOfButtons).style.display = "none";
-        if (compteur == 0){
-        document.getElementById("step1").innerHTML = "Identify 4 things you see"; 
-        } else if (compteur == 1){
-         document.getElementById("step1").innerHTML = "Identify 3 things you see";   
-        } else if (compteur ==2){
-         document.getElementById("step1").innerHTML = "Identify 2 things you see";
-        } else if (compteur ==3){
-         document.getElementById("step1").innerHTML = "Identify 1 things you see";
-        }
+        event.preventDefault(); 
+        event.currentTarget.setAttribute('class', 'thebtn2');
+        //console.log(event.currentTarget);
+        // //insert code here
+        // // Update state on button click
+        //  answered["button" + this._id] = true;
+        // // Have all buttons been clicked?
+        // var allTrue = Object.keys(answered).every(function(key){ return answered[key] === true });
+        // if (allTrue) {
+        // // Remove a button and reset state
+        // document.getElementById("button" + numberOfButtons).style.display = "none";
+        // if (compteur == 0){
+        // document.getElementById("step1").innerHTML = "Identify 4 things you see"; 
+        // } else if (compteur == 1){
+        //  document.getElementById("step1").innerHTML = "Identify 3 things you see";   
+        // } else if (compteur ==2){
+        //  document.getElementById("step1").innerHTML = "Identify 2 things you see";
+        // } else if (compteur ==3){
+        //  document.getElementById("step1").innerHTML = "Identify 1 things you see";
+        // }
       
-        delete answered["button" + numberOfButtons];
-        Object.keys(answered).forEach(function(key){ answered[key] = "false" });
-        numberOfButtons--;
-        compteur ++;
-        };
+        // delete answered["button" + numberOfButtons];
+        // Object.keys(answered).forEach(function(key){ answered[key] = "false" });
+        // numberOfButtons--;
+        // compteur ++;
+        // };
     }
 });
 
