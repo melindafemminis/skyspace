05e91bb13982a86de66050f255928539d5f77994
Kykka90 <44426484+Kykka90@users.noreply.github.com>
2019-04-10 09:30:56 +0200

    Quelques modifications de style

--- client/ui/home/home.js
+++ client/ui/home/home.js
@@ -2,6 +2,11 @@ import './home.html';
 import './homeFond.css';
 import './homeFond.html';
 
+//changer la couleur de fond du body
+Template.fond.rendered = function(){
+    $('body').removeClass('nuit');
+}
+
 Template.home.events({
     'click #startMethod'(event, instance) {
         event.preventDefault();  
