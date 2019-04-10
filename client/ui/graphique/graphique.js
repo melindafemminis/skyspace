c171cf613011f5476115d22383474b655ad7bfa3
Melinda <femminis.mda@gmail.com>
2019-04-10 21:19:51 +0200

    Ajout de la fonction supprimer les données
    Ajout de la fonction supprimer les données des collections humeurDebut et humeurFin.
    Il faut encore résoudre le problème que du côté client, on peut seulement supprimer les document des bases de données par _id, donc un a un...

--- client/ui/graphique/graphique.js
+++ client/ui/graphique/graphique.js
@@ -45,26 +45,12 @@ Template.graphique.events({
                 datasets: [{
                     label: 'humeurDebut',
                     data: arrValue,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
+                    backgroundColor: "rgba(192, 192, 192,0.5)",
+                    borderColor: "rgba(192, 192, 192,0.5)",
                     borderWidth: 1,
-                    backgroundColor: "rgba(240,251,255,0.5)",
+                    backgroundColor: "rgba(192, 192, 192,0.5)",
                     borderColor: "rgba(240,251,255,1)",
-                    fontColor: "rgba(240,251,255,0.5)",
+                    fontColor: "rgba(192, 192, 192,0.5)",
                     
                 }]
             },
@@ -85,63 +71,18 @@ Template.graphique.events({
 
 
 
-// Template.graphique.events({
-//   'click #graphBtn': function(){
-//     var ctx = $('#myChart');
-//     Chart.defaults.global.defaultFontColor = "rgba(240,251,255,1)",
-// console.log(ctx);
-// var myChart = new Chart(ctx, {
-    
-//     type: 'line',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1,
-//             backgroundColor: "rgba(240,251,255,0.5)",
-//             borderColor: "rgba(240,251,255,1)",
-//             fontColor: "rgba(240,251,255,0.5)",
+////////////////////////////////////////////////////////////////////////////////////
+/// D E L E T E   D A T A
+////////////////////////////////////////////////////////////////////////////////////
+
+
+Template.graphique.events({
+    'click #delete': function(){
+        if (confirm('Effacer toutes les données ? Cette action est irréversible.')) {
+            humeurDebut.remove({});
+            humeurFin.remove({});
             
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
-//   }
-// });
-
-
-
-// let myArray = [];
-
-// Template.graphique.events({
-//     'click #graphBtn': function(){
-//         humeurDebut.find().forEach( function(myDoc) { myArray.push(myDoc.value ); } );
-//         document.getElementById('leDiv').innerHTML = myArray;        
-//     }
-// });
+        } 
+    }
+});
 
