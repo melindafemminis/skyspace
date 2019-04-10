8efb6c07863c4bb6a16513437167d00d6160b23c
Melinda <femminis.mda@gmail.com>
2019-04-10 15:53:48 +0200

    Slider boutons
    Séparation des boutons du template slider dans 2 templates différents (sliderBtn1 et sliderBtn2) afin que le bouton "suivant" enregistre les données en 2 collections différentes.

---
+++ client/ui/sliderBtn1/sliderBtn1.js
@@ -0,0 +1,17 @@
+import './sliderBtn1.html'
+
+Template.sliderBtn1.events({
+    //Code pour définir ce qu'il se passe quand le bouton 'suivant' est cliqué
+    'click #sliderNextButton'(event, instance) {
+      event.preventDefault();
+      //Insérer la valeur du slider + date dans la collection humeurDebut
+      humeurDebut.insert({
+        value: document.getElementById("rangeSlider").value,
+        createdAt: new Date()
+      })
+      //Simple console log de la valeur du slider pour voir que ça marche
+      console.log(document.getElementById("rangeSlider").value);
+      //Changer de page 
+      FlowRouter.go('theMethod');
+    }
+});
---
+++ client/ui/sliderBtn2/sliderBtn2.js
@@ -0,0 +1,1 @@
+import './sliderBtn2.html';
