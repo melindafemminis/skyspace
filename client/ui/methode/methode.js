2c7eba1ac6d8a48c44187aeb802c72f9a64bb7f9
Kykka90 <44426484+Kykka90@users.noreply.github.com>
2019-04-13 20:04:37 +0200

    Des petites modifications de style

--- client/ui/methode/methode.js
+++ client/ui/methode/methode.js
@@ -1,6 +1,8 @@
 import './methode.html';
 import './methodeFond.html';
 import './methodeFond.css';
+import './etoiles.html';
+
 
 //changer la couleur de fond du body
 Template.fondmethode.rendered = function(){
