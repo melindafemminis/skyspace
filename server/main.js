82c24ebf7c7fe18e04ead983f47d10ea302902f8
Melinda <femminis.mda@gmail.com>
2019-04-16 14:58:47 +0200

    Ne pas faire confiance au client
    Suppression du package insecure, création de la page methods.js et empêcher la modification du  champ profile.pseudo.

--- server/main.js
+++ server/main.js
@@ -3,4 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import '../both';
 import '../both/collections';
 
-
+//Empêcher le client de pouvoir modifier la collection Meteor.users
+Meteor.users.deny({
+    update() { return true; }
+});
