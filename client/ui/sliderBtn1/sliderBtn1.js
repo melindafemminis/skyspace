bdd63b136ec920c2b29747d04eb8830bb5295c5d
Melinda <femminis.mda@gmail.com>
2019-04-17 13:17:07 +0200

    comm

--- client/ui/sliderBtn1/sliderBtn1.js
+++ client/ui/sliderBtn1/sliderBtn1.js
@@ -1,8 +1,10 @@
 import './sliderBtn1.html'
 
 Template.sliderBtn1.events({
+
     //Code pour définir ce qu'il se passe quand le bouton 'suivant' est cliqué
     'click #sliderNextButton'(event, instance) {
+      
       event.preventDefault();
       //Insérer la valeur du slider + date dans la collection humeurDebut
       humeurDebut.insert({
