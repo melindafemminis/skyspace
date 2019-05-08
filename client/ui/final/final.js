e445acd008d4e0189bae1891ddd16df0c9501d68
kenan1024 <kenan.yildiz@unil.ch>
2019-05-08 12:39:40 +0200

    logo, random quotes,

--- client/ui/final/final.js
+++ client/ui/final/final.js
@@ -20,39 +20,26 @@ Template.final.helpers({
             "Oublie ce qui t’as blessé mais n’oublie pas ce que cela t’as appris",
             "Ces erreurs et ces fautes sont ce que je suis, dessinant les plus belles étoiles dans la constellation de ma vie",
             "N’aie pas peur d’échouer, aie peur de ne pas essayer",
-        ]
+            "Plus la montée est dure, plus la vue sera belle",
+            "Le pessimiste voit la difficulté dans chaque opportunité. L’optimiste voit une opportunité dans chaque difficulté",
+            "Notre plus grande gloire n’est pas de jamais tomber, mais de se relever chaque fois que nous tombons",
+            "10% de ta vie sont ce qui t’arrive, 90% comme tu y réagis",
+    ]
+
         let quotesNegativ = [
-            "Plus la montée est dure, plus la vue sera belle",
-            "Le pessimiste voit la difficulté dans chaque opportunité. L’optimiste voit une opportunité dans chaque difficulté",
-            "Notre plus grande gloire n’est pas de jamais tomber, mais de se relever chaque fois que nous tombons",
-            "Notre plus grande gloire n’est pas de jamais tomber, mais de se relever chaque fois que nous tombons",
-            "10% de ta vie sont ce qui t’arrive, 90% comme tu y réagis",
             "La vitesse à laquelle tu avances n’a pas d’importance tant que tu ne t’arrête pas",
             "La seule fois que tu devrais regarder derrière toi est pour voir le chemin que tu as fait",
             "Voir c’est savoir, vouloir c’est pouvoir, oser c’est avoir",
+            "Il faut viser la lune, parce qu’au moins, si vous échouez, vous finirez dans les étoiles",
+            "Le seul endroit où le succès précède le travail est dans le dictionnaire",
+            "Pour pouvoir contempler un arc-en-ciel, il faut d’abord endurer la pluie",
+            "L’échec est l’épice qui donne sa saveur au succès",
         ]
 
-        let quotesEqual = [
-            "Plus la montée est dure, plus la vue sera belle",
-            "Le pessimiste voit la difficulté dans chaque opportunité. L’optimiste voit une opportunité dans chaque difficulté",
-            "Notre plus grande gloire n’est pas de jamais tomber, mais de se relever chaque fois que nous tombons",
-            "Notre plus grande gloire n’est pas de jamais tomber, mais de se relever chaque fois que nous tombons",
-            "10% de ta vie sont ce qui t’arrive, 90% comme tu y réagis",
-            "La vitesse à laquelle tu avances n’a pas d’importance tant que tu ne t’arrête pas",
-            "La seule fois que tu devrais regarder derrière toi est pour voir le chemin que tu as fait",
-            "Voir c’est savoir, vouloir c’est pouvoir, oser c’est avoir",
-        ]
 
-        //https://docs.meteor.com/api/collections.html#sortspecifiers
-        var hd = humeurDebut.findOne({}, {sort:[['createdAt', 'desc']]}).value;
-        var hf = humeurFin.findOne({}, {sort:[['createdAt', 'desc']]}).value;
-        
-        if(hd<hf){
+        if(humeurDebut<humeurFin){
             return quotesPositiv[Math.floor(Math.random() * quotesPositiv.length)];
-        }else if(hd==hf){
-            return quotesEqual[Math.floor(Math.random() * quotesEqual.length)];
-        }
-        else {
+        }else {
             return quotesNegativ[Math.floor(Math.random() * quotesNegativ.length)];
         }
     }
