ea0c87c868fb7f98e382bbd3b2de774d712408a7
Melinda <femminis.mda@gmail.com>
2019-05-23 12:09:58 +0200

    Sound: ajout de la méthode musicOn/musicOff lors du click sur l'image speaker
    L'ajout de la méthode musicOn/musicOff pour que l'attribut music dans user-profile change aussi quand l'utilisateur clique sur le bouton de la musique en bas à gauche, depuis n'importe ou dans l'application.

--- client/ui/soundPlayer/soundPlayer.js
+++ client/ui/soundPlayer/soundPlayer.js
@@ -76,12 +76,14 @@ Template.fond.audioMute = function(){
         document.getElementById('soundButton').src = "/volume.png";
         Template.fond.isMute = false;
         Template.fond.audioPlay(); //on démarre la musique
-        document.getElementById("switchMusic").checked = true;
+        document.getElementById("radioMusic").checked = true;
+        Meteor.call('musicOn');
     }else{
         Template.fond.isMute = true;
         document.getElementById('soundButton').src = "/volumeMute.png";
         Template.fond.audio.pause(); //on pause la musique
-        document.getElementById("switchMusic").checked = false;
+        document.getElementById("radioMusic").checked = false;
+        Meteor.call('musicOff');
     }
 }
 
